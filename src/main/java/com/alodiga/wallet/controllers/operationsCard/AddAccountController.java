e6b9185b93de1b57105d19be5aae01d3acfdb40d
angel <klousinmiranda@gmail.com>
2022-03-07 09:21:45 -0400

    Ajuste en agregar cuenta bancaria

--- src/main/java/com/alodiga/wallet/controllers/operationsCard/AddAccountController.java
+++ src/main/java/com/alodiga/wallet/controllers/operationsCard/AddAccountController.java
@@ -1,14 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.alodiga.wallet.controllers.operationsCard;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import com.alodiga.cms.commons.ejb.PersonEJB;
 import com.alodiga.cms.ws.APIAuthorizerCardManagementSystemProxy;
 import com.alodiga.wallet.common.ejb.BusinessPortalEJB;
