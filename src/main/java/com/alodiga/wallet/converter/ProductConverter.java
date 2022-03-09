fd63ba5d5567e9b5959848179c49b3c4c2115e78
Jesús Gómez <jedugomvil@hotmail.com>
2022-03-09 11:21:39 -0400

    Ya guarda la solicitud de recarga manual

--- src/main/java/com/alodiga/wallet/converter/ProductConverter.java
+++ src/main/java/com/alodiga/wallet/converter/ProductConverter.java
@@ -5,10 +5,19 @@
  */
 package com.alodiga.wallet.converter;
 
-import com.alodiga.wallet.ws.Product;
+import com.alodiga.wallet.common.ejb.ProductEJB;
+import com.alodiga.wallet.common.exception.GeneralException;
+import com.alodiga.wallet.common.exception.NullParameterException;
+import com.alodiga.wallet.common.exception.RegisterNotFoundException;
+import com.alodiga.wallet.common.genericEJB.EJBRequest;
+import com.alodiga.wallet.common.utils.EJBServiceLocator;
+import com.alodiga.wallet.common.utils.EjbConstants;
+import com.alodiga.wallet.controllers.operationsCard.ManualRechargeRequestController;
+import com.alodiga.wallet.common.model.Product;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -19,25 +28,29 @@ import javax.faces.convert.FacesConverter;
  *
  * @author jgomez
  */
-@ManagedBean(name = "productConverter")
-@ViewScoped
+@FacesConverter("productConverter")
 public class ProductConverter implements Converter {
     
     @Override
     public Object getAsObject(FacesContext facesContext, UIComponent component, String submittedValue) {
+        Product selectedProduct = null;
         if (submittedValue == null || submittedValue.equals("")) {
             return "";
         }
-        try {
-            long idProduct = Long.parseLong(submittedValue);
-            Product product = new Product();
-            product.setId(idProduct);
-            return product;
-        } catch (NumberFormatException ex) {
+        try { 
+            ProductEJB  productEJB = (ProductEJB) EJBServiceLocator.getInstance().get(EjbConstants.PRODUCT_EJB);
+            EJBRequest request = new EJBRequest();
+            request.setParam(Long.parseLong(submittedValue));
+            selectedProduct = productEJB.loadProduct(request);
+        } catch (RegisterNotFoundException ex) {
+            Logger.getLogger(ProductConverter.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NullParameterException ex) {
+            Logger.getLogger(ProductConverter.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (GeneralException ex) {
+            ex.printStackTrace();
             Logger.getLogger(ProductConverter.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        return null;
+        return selectedProduct;
     }
 
     @Override
@@ -53,5 +66,5 @@ public class ProductConverter implements Converter {
 
         }
     }
-    
+   
 }
